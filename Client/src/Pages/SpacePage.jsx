3c49d8700648eb9621b08a98af14d21ab1eb8f7f
Kshitij Kotecha <kotechakshitij@gmail.com>
2024-08-09 10:21:52 +0530

    Space Page made and modified Dashoard

--- Client/src/Pages/SpacePage.jsx
+++ Client/src/Pages/SpacePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import "./Styles/SpacePage.css";
 
 const SpacePage = () => {
   const { publicUrl } = useParams();
