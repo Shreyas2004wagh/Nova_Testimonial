401ca23d18880a2c40ae27d6a94e0e92dceb62ad
Shreyas2004wagh <shreyaswagh2004@gmail.com>
2024-08-13 13:24:22 +0530

    changed local host link with render link

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5000/login', {
+      const response = await axios.post('https://nova-testimonial.onrender.com/login', {
         email: form.email,
         password: form.password,
       });
